c921d1840a458a33d7814ce570a03759886fdccb
AshakaE <ashakaegerega@gmail.com>
2021-02-05 16:21:55 +0100

    Using Strong Parameters

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,4 +19,9 @@ class ArticlesController < ApplicationController
       render :new
     end
   end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
